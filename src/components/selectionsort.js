bf359e75ffd17fbbdc7f0b5375da8290bbacf43f
granth2023 <grantharris38@gmail.com>
2024-02-29 10:51:35 -0500

    selection sort

---
+++ src/components/selectionsort.js
@@ -0,0 +1,8 @@
+
+
+<h2>
+
+    while unsorted: how do we search iteratively through the unsorted part of data and find the lowest value? How do we swap the smallest value found with the first element of the unsorted segment? Why is this the worst?  What is the time complexity? What is the space complexity? What is the stability? What type of sort is this?  
+
+
+</h2>
--- src/pages/sort.js
+++ src/pages/sort.js
@@ -6,7 +6,14 @@
 
 
 <h3>
-    
+    Why sort? What is easier to do when things are sorted? How do you sort files? 
+
+    How many possible ways could we sort? 
+
+    What is runtime complexity sort? 
+    What is space complexity sort? 
+
+    What is the worst sort? 
 
 
 </h3>
