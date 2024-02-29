57f6f8a6d619a672ef950cbe133039f63f03c9ed
granth2023 <grantharris38@gmail.com>
2024-02-29 11:16:04 -0500

    add bubble sort

---
+++ src/components/bubblesort.js
@@ -0,0 +1,12 @@
+
+
+<h2>
+    what is bubble sort? what does it do? how does it bubble up higher values? How does it push down lower values? 
+
+    what is a swap counter? it increments when you compare adjacent values. if it stays at zero then we're there.  
+
+    how does it detect completion 
+
+    
+
+</h2>
--- src/components/selectionsort.js
+++ src/components/selectionsort.js
@@ -2,7 +2,18 @@
 
 <h2>
 
-    while unsorted: how do we search iteratively through the unsorted part of data and find the lowest value? How do we swap the smallest value found with the first element of the unsorted segment? Why is this the worst?  What is the time complexity? What is the space complexity? What is the stability? What type of sort is this?  
+    while unsorted: how do we search iteratively through the unsorted part of data and find the lowest value? How do we swap the smallest value found with the first element of the unsorted segment? 
+    //is this the lowest value? 
+    is this the lowest value? 
+    no. 
+    is this the lowest value? 
+    no. 
+    Is this? 
+    Yes. 
+    
+    Why is this the worst? 
+    
+     What is the time complexity? What is the space complexity? What is the stability? What type of sort is this?  
 
 
 </h2>
