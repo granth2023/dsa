c0291ee5f049e2bc8fc75e806a3ddd86a1227c14
granth2023 <grantharris38@gmail.com>
2024-02-29 11:31:17 -0500

    buble sort pseudo code

--- src/components/bubblesort.js
+++ src/components/bubblesort.js
@@ -15,3 +15,32 @@
 
 
 </h2>
+
+function bubbleSort(arr) {
+    //create a swap counter   
+    let swapCounter;
+
+    do {
+        //start the count at zero 
+        swapCounter = 0;
+        //start sorting through the data through the data  
+        for ( let i = 0; i < arr.length -1; i++ ) {
+
+            // if the current number we are looped at, is greater than the next number -- 
+            if( arr[i] > arr[i + 1] ) {
+                //set the current number inside the temp variable  and --
+                let temp = arr[i]
+                //set the bigger number to the location of the current number aka swap so temp
+                arr[i] = arr[i +1];
+                //goes to the place where it belongs sorted wise 
+                arr[i + 1] = temp;
+                //increase the swap counter for this occuring 
+                swapCounter++;
+            }
+    }
+    //continue the looping while the counter does not equal zero
+    } while (swapCounter !== 0);
+
+    return arr;
+
+}
