f40fe4cf6599fe6c5c0a31f8dcbdaf357f4729e3
granth2023 <grantharris38@gmail.com>
2024-02-29 11:44:54 -0500

    insertion sort pseudo

---
+++ src/components/insertionsort.js
@@ -0,0 +1,32 @@
+<h2>
+    does it work well on big data or small collection? 
+
+    how do we psuedo code? 
+
+    how do we handle the first item? can we say its sorteD? 
+
+    what do we do next? how do we look at the next element? do we treat it as unsorted? what does that mean? what do we do with it? 
+
+    can we insert the element into the sorted portion? do we evalute which? do we evalute the sorted portion? where does the sorted portion go? 
+
+    how do we pull elements out and compare it? how do we put it back into place? 
+
+    what does it mean to consider the first item in the array as sorted in context of the insertion sort? what does this assume? if the first item in the array is sorted, what is the rest of the array? 
+
+    is a single element by itself inherently sorted?  
+    what are we doing with the unsorted portion? how do we insert them into the sorted portion?
+
+    
+
+
+
+</h2>
+
+
+function insertionSort (arr){
+
+
+
+
+
+}
