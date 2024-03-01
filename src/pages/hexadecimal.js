3ccd05654b172e472e5fcf2a2390514e38db3752
granth2023 <grantharris38@gmail.com>
2024-02-29 19:34:43 -0500

    hexadecimal

---
+++ src/pages/hexadecimal.js
@@ -0,0 +1,15 @@
+
+
+<h2> 
+    how does 16 base work? 
+
+    how does 0 in hexadecmial and binary work? what is 0? 
+
+    how does a number work? how does it work? a number is 2 based and there are 8 slots so to work backwards we need a number -- 128 is the max? 
+
+    because 2 to the 4th is 16, 2 hexdecimal digits represent 1 byte, one hexdecimal digit reprsetn 4 bits  
+
+    
+
+
+</h2>
