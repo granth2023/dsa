fc3ec3f458a4a217566b4bb156b72a6a4c4469fa
granth2023 <grantharris38@gmail.com>
2024-02-29 19:55:34 -0500

    241

--- src/pages/hexadecimal.js
+++ src/pages/hexadecimal.js
@@ -9,7 +9,40 @@
 
     because 2 to the 4th is 16, 2 hexdecimal digits represent 1 byte, one hexdecimal digit reprsetn 4 bits  
 
-    
+what is 17 in hexadecimal? 16 to the first plus 16 to the zero 
+
+what is one in hexa ? 16 to zero    
+what is two? 2 ""
+but how does it look binary? 
+
+remember a 16 to the zero is broken down with 2 to the 0 1 2 3 4 so to get to two is 0010 in hexacedimaly and binary 
+
+so three is 0011
+so four is 0100 
+
+wha t is nine -- 16 to the zero and its 1001
+10 is 1010
+
+12 is 1100
+15 is 1111 
+
+what is 241
+so if you need to go past 15 you now go into a seccond four squad you need to do 16 to the first and if we rememebr it goes back to 0 - 9 then the letters 
+
+so what would it be to 241? 
+
+you need at least 16 to 
+which is 2 to 45,6,7,
+
+so it would be 
+
+1 1 1 1 0 0 0 1
+
+what number is this? 
+
+// think of it as directing 
+
+
 
 
 </h2>
