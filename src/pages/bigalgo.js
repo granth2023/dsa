5f42d2f38ad3722d381577897e1a11ee8a91946f
granth2023 <grantharris38@gmail.com>
2024-02-29 22:22:34 -0500

    big o

---
+++ src/pages/bigalgo.js
@@ -0,0 +1,123 @@
+<h2>
+
+    what is equivalent ot O(N)
+    o(2n)
+what are all the terms for classifciation? what is time complexity? what is space complexity?
+
+what is space? why do vast majority of data structures hav ea space complexity of O (N)?
+
+what is O (n)?
+
+what is o1? 
+how do you describe the nature of space taken up in a computer and the size of data? 
+
+What is factorial complexity? 
+
+what is power of 2 complexity? 
+
+what is log linear complexity? 
+
+what are runtimes? 
+what is a multi part algo? how does it deal with multiple data sets? do we add or multiply runtimes? when? 
+
+why add them if its a do this then wehn done do that
+
+how is runtime influenced by sieze of arr and arr  how do we big o represent this or how do we abstrac?
+
+when do we multiply? 
+why is it when we do this for each time you do that then mulitply? 
+
+why do we multiplate when we iterate through arr b for every time in arr A 
+
+why multiple when we iterate through a for every item in a 
+
+so is it arr a times arr a or arr squared?
+what do we do with the b  array? do we add that? what is the abstraciton? 
+
+
+
+what are dropping non dominant terms? 
+--once youre in the quadrtic it sover
+
+
+what is quadratic complexity? 
+O(N^2) 
+
+how is input affected? is it efficent? examples? why quick, buble, and insertion? 
+nested loops? 
+
+1 item = 1 second
+100 items = ? 
+
+how is looping through 2 d array? how is this code doing that? 
+
+
+what is logarithimc complexity? 
+O(log(N))
+hwo does input growth affect process time? 
+why half? 
+are they efficient? 
+what are examples? 
+why binary search? 
+why a guess teh number? 
+you are taking things out fo the equation-- its like an algorithim where you are taking away from the total even when you guess wrong
+
+if it takes 1 second for 1 item,s how long for 100 items? 
+
+it processes increases half of what it was previously used?
+16 million to 1 in 24 iterations
+
+
+what are droping constants? 
+does the amount of code matter for the same data? 
+what is the point of big o in realtion to here? 
+in big o does it matter if they arent the same? 
+why does it matter how it scales? 
+what about the data is different? nothing so it scales the same
+150N is the same as N why? 
+its all scaling the same 
+
+
+what is linear complexity? 
+O(N)
+how does input effect the pace? 
+is this considered efficeint? 
+examples? 
+why searching an array? 
+why on all ements? 
+what about inserting single things into existing data? 
+is downloading? 
+
+if it takes 1 second to process 100 items takes how long? 
+
+what is constnat complexity? 
+0(1)
+how does the input matter? 
+will it take the same time? 
+
+what are some expamples? 
+why add or remove single elements? 
+why on one digit? 
+
+if it takes item 1 second to process, how long would 100 items? 
+
+what's a code exmpale ?
+
+what is big o? 
+how do we describe efficiency of an algorithim? 
+how many cpu cycles does it take vs actual amount of time it takes vs how many iterations? 
+what does big o meausre? does it account of r small picture or unit of measurement? what metric is it concerned with? how is it for detail? 
+what is a runtime? 
+how do we judge exactly what makes an lgorithm faster slower and more or less space efficient than a similar algo? how does it improve our decisions? 
+
+what is the best case version? 
+is it lucky to get omega? 
+
+what is representation for expectated case? what symbol? 
+
+
+what are alogirithms
+how do we achieve a goal or solve? show oven 
+
+
+</h2>
--- src/pages/hexadecimal.js
+++ src/pages/hexadecimal.js
@@ -40,7 +40,144 @@ so it would be
 
 what number is this? 
 
+why do we do this? 
+hexadecimal makes binary nums more human dreadbale? 
+
+wifi address: F0:C3:71:53:12:6D
+what number is this hexideciaml 
+
+what else has hexidcecimal? do error codes? 
+what about addresses? 
+
+what about CSS? 
+
+what is rgb ? what is a 1 byte hex? does it represent ad decimal number from 0-255? 
+
+what does rgb represent? 
+
+how do we make a color set that has values of each byte in the hex color from 00 up to ff 
+
+what does a 255, 0 0  in #? ff is 255, all red what is red? what is green? what is blue? what would yellow be? what would light blue? what about pink? 
+
+how does emre do it? 
+
+what number is this? 
+
+11110001
+
+what color is this? ##FF0000
+
+today we are learning how to read binary -- why? read wifi addresses? its the language of computer memory? it's data
+
+today we will learn to read that 
+we want to read it because it is how computer memory represents data -- at its core 
+if we can begin to understand the foudnation of the computer, we can work our way up from the bottom. 
+so
+binary numbers 
+
 // think of it as directing 
+directing is a perfomrance - teaching-- explain-- director 
+
+what is base-10?
+1 2 8 
+10 square plus 10 to the first plus 10 to the zero 
+
+what is base 2? does it make sense to call it binary? 
+
+so is its base -2 and not 10 what is our base? what replace 10? 
+
+how do we get to 128
+
+how much is a byte? MENTION EARLIER
+
+why is this 128? 
+
+how do we get 43? 
+
+again why? mention earlier! 
+
+networking
+character sets
+optical storage
+processing? 
+file encoding
+digital audio
+displays/images/video 
+
+i spent about 3 hours in the morning and another 2 in the evening 
+im taking hours this week 
+
+what about ip address -- four dot seperated bytes? how many is that? 
+how do we find the binary representation of 10.0.119.42
+
+what about character sets? 
+what is ascii 
+what about the english alphabet-- how is that reprsented
+how many bits? how many characters? how many printable? any edge cases? 
+what is A? 
+lets watch this video? 
+
+what is hexadecimal? 
+
+what is base 2? what is base 10? what is base -16? 
+
+how do we represent? 
+why do we use 16
+
+how is the same as 2 or 10? 
+
+so what are the palce in 16? 
+
+so what do the digits of a hexadecimal represent? 
+what makes up 1 byte? 
+how many bits make up a byte? 
+what is 17? 
+why is 17? 
+
+what is hexadecimal? 
+what is binary? 
+
+binary is 8 
+or two hexadecimal 
+
+one hexadecimal represnts 4 digits 
+1 is the same as 0001 
+
+16 to the zero is 1
+so to get to 1 we need the first four powers of 2 and only need the first one for 1 
+
+to get to 17 
+
+we need the 1 
+and then the first digit in the second hexa which is 16 so the fifth power of 2 
+
+
+what is 10 in hexadecimal? a
+
+
+css color
+what is rgb stand for? 
+what are the numbers stand for 
+what is 255? is the the number the highest? 
+about about lowest? 
+how about that in hex? what does that look like? how do we represnt 255 in hex? 
+
+still need more prep! 
+
+are they values? what are they expressed in? what is base 16? how is it represented? what is a hexadecimal? 
+how many hours can I log? 
+2 hours thinking about contract started on february 26 so ill log 20
+
+
+
+
+where is base-2? 
+
+
+
+
+
+
 
 
 
