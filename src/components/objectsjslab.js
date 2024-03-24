005fbc03d01a311f1b9d18795a8d3704662875cc
granth2023 <grantharris38@gmail.com>
2024-03-24 19:54:42 -0400

    objects work

--- src/components/objectsjslab.js
+++ src/components/objectsjslab.js
@@ -124,13 +124,13 @@ console.log(title)
   
   // Exercise 8:  Using the talkingHeadsAlbums array, assign album5's labels property to a variable named labels
   labels = album5.albumDetails.labels
-  
+  console.log(labels)
   
   
   // Exercise 9:  Using the talkingHeadsAlbums array, assign album7's released property to album6's released property
+  property = talkingHeadsAlbums[6].albumDetails.released
   
-  
-  
+  console.log(property)
   
   // Exercise 10:  Using the pre-defined variable named albumIdx below, assign the albumDetails object of the album located within the talkingHeadsAlbums array at the index represented by the value of albumIdx to a variable named albumDetails
   
