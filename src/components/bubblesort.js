e649f7c7b3c6ac90eba17025bf919d2857bf91cf
granth2023 <grantharris38@gmail.com>
2024-03-27 19:07:57 -0400

    start sum array

--- src/components/bubblesort.js
+++ src/components/bubblesort.js
@@ -17,17 +17,18 @@
 
 
 </h2>
-
+arr = [5, 3, 10, 4, 2, 7, 8,3]
 function bubbleSort(arr) {
     //how do we keep track of how many times we swap? also we need to go through this a bunch of times and then  
     let swapCounter;
+    let runs;
 
     do {
         //swhat do we start the count at? 
         swapCounter = 0;
         //how do we start going through the data?   
-        for ( let i = 0; i < arr.length -1; i++ ) {
-
+        for ( let i = 0; i < arr.length -1 - runs ; i++ ) {
+            runs = 0
             //what are we comparing? 
             if( arr[i] > arr[i + 1] ) {
                //how do we create the possibility of change? 
@@ -38,6 +39,7 @@ function bubbleSort(arr) {
                 arr[i + 1] = temp;
                 //if this occurs do we increment our swapcounter? 
                 swapCounter++;
+                runs++
             }
     }
     //how do we keep this going? what if we need to keep swapping? when do we stop swapping? 
@@ -46,6 +48,8 @@ function bubbleSort(arr) {
     return arr;
 
 }
+console.log(bubbleSort(arr))
+
 //what are the passes? its like its only comparing the next thing, so we dont know once a thing is swapped, its good! what about what's next ot it now? 
 
 // each pass increments a swap counter and then boom. if not zero send it back. 
