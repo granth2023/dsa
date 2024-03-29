9cbea71d6bf4af8486b6bfaee06e2c92661aa805
granth2023 <grantharris38@gmail.com>
2024-03-28 20:33:20 -0400

    pseudo code binary serach algo

--- src/pages/search.js
+++ src/pages/search.js
@@ -1,1 +1,31 @@
 //linear search is what? how is a for loop linear? what about finding a value wihtin an array for loop is linear? 
+
+// what are some characteristics of a linear search? 
+
+//why o of n? 
+
+//can they be done on sorted or unsorted? why? 
+
+// can binary be done on sorted or unsorted? why? 
+
+// first, get the starting index and ending index of a collection -- how do we do that? 
+
+function binarySearch(){
+
+
+    //how do we finthe midpoint by adding idnexes together and diving by 2? 
+
+
+    //how do we write the fact that while we have no found the element and not exhauseted the serach
+
+    // how do we serach to the left of the midpoint? to the right? 
+
+    //why repeat athe midpoint calucalition start and end index over 2? 
+
+    //how do we return the idnex if found? if not how do we return the bitwise not of the start index? 
+
+
+}
+
+
+//what are characteristic
