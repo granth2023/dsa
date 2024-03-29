069697c56cc247f8993d42c2c124995eeaacfc97
granth2023 <grantharris38@gmail.com>
2024-03-28 20:55:00 -0400

    attempt made

--- src/pages/search.js
+++ src/pages/search.js
@@ -9,22 +9,50 @@
 // can binary be done on sorted or unsorted? why? 
 
 // first, get the starting index and ending index of a collection -- how do we do that? 
-
-function binarySearch(){
+//what do we pass in the binary serach? is it the array and the value we want to find? 
+function binarySearch(arr, x){
+    let start = 0 
+    let end = arr.length -1
 
 
     //how do we finthe midpoint by adding idnexes together and diving by 2? 
+    let midpoint = (start +end)/2;
 
+    if (x = midpoint[x]){
+        return x;
+    }
+    while (midpoint != x || start != x || end !=x){
+
+
+    if ( x < midpoint ){
+        midpoint = (start + midpoint -1)/2
+
+    } else  {
+        midpoint = (midpoint +1 + end)/ 2
+
+    }
+    }
+    return arr[x];
+}
 
     //how do we write the fact that while we have no found the element and not exhauseted the serach
 
+
+    while( x != arr[i]){
+
+        function find(x) {
+
     // how do we serach to the left of the midpoint? to the right? 
+    if(x !=  arr[0] || x != midpoint -1 ){
+        submidpoint = (arr[0] / midpoint -1 )/ 2 
+        find()
+    }
 
     //why repeat athe midpoint calucalition start and end index over 2? 
 
     //how do we return the idnex if found? if not how do we return the bitwise not of the start index? 
 
-
+    }
 }
 
 
