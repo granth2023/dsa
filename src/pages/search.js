2789bf4c14c692e28d9861c80b30face713f696a
granth2023 <grantharris38@gmail.com>
2024-03-28 20:58:17 -0400

    pseudo code is there for tomorrow

--- src/pages/search.js
+++ src/pages/search.js
@@ -10,50 +10,50 @@
 
 // first, get the starting index and ending index of a collection -- how do we do that? 
 //what do we pass in the binary serach? is it the array and the value we want to find? 
-function binarySearch(arr, x){
-    let start = 0 
-    let end = arr.length -1
+// function binarySearch(arr, x){
+//     let start = 0 
+//     let end = arr.length -1
 
 
-    //how do we finthe midpoint by adding idnexes together and diving by 2? 
-    let midpoint = (start +end)/2;
+//     //how do we finthe midpoint by adding idnexes together and diving by 2? 
+//     let midpoint = (start +end)/2;
 
-    if (x = midpoint[x]){
-        return x;
-    }
-    while (midpoint != x || start != x || end !=x){
-
-
-    if ( x < midpoint ){
-        midpoint = (start + midpoint -1)/2
-
-    } else  {
-        midpoint = (midpoint +1 + end)/ 2
-
-    }
-    }
-    return arr[x];
-}
-
-    //how do we write the fact that while we have no found the element and not exhauseted the serach
+//     if (x = midpoint[x]){
+//         return x;
+//     }
+//     while (midpoint != x || start != x || end !=x){
 
 
-    while( x != arr[i]){
+//     if ( x < midpoint ){
+//         midpoint = (start + midpoint -1)/2
 
-        function find(x) {
+//     } else  {
+//         midpoint = (midpoint +1 + end)/ 2
 
-    // how do we serach to the left of the midpoint? to the right? 
-    if(x !=  arr[0] || x != midpoint -1 ){
-        submidpoint = (arr[0] / midpoint -1 )/ 2 
-        find()
-    }
+//     }
+//     }
+//     return arr[x];
+// }
 
-    //why repeat athe midpoint calucalition start and end index over 2? 
+//     //how do we write the fact that while we have no found the element and not exhauseted the serach
 
-    //how do we return the idnex if found? if not how do we return the bitwise not of the start index? 
 
-    }
-}
+//     while( x != arr[i]){
+
+//         function find(x) {
+
+//     // how do we serach to the left of the midpoint? to the right? 
+//     if(x !=  arr[0] || x != midpoint -1 ){
+//         submidpoint = (arr[0] / midpoint -1 )/ 2 
+//         find()
+//     }
+
+//     //why repeat athe midpoint calucalition start and end index over 2? 
+
+//     //how do we return the idnex if found? if not how do we return the bitwise not of the start index? 
+
+//     }
+// }
 
 
 //what are characteristics of binary? 
@@ -63,3 +63,20 @@ function binarySearch(arr, x){
 //how do we index and serach mroe complex elemnts in a colelction? 
 //what is interpolation? 
 
+//second attempt 
+
+
+//declare and pass the specific value and where it belongs to 
+
+//save the first value and the last value in the set 
+
+
+// while our start is less than or equal to our end, we want to divide the data set in half and save that 
+
+//if the value of our midpoint is our value then we can return that midpoint
+
+//if our value at midpoint is less than our value we want to look to the right of the midpoint and keep dividng 
+
+//if our value is greater than x we want to look to the left and resave the end value 
+
+//if the value is not there is the data set just give us bitwise
