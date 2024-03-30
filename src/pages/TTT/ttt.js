9358ca45fba211383b168b5dc4543c1debf0c78a
granth2023 <grantharris38@gmail.com>
2024-03-30 13:11:46 -0400

    declared variables

--- src/pages/TTT/ttt.js
+++ src/pages/TTT/ttt.js
@@ -3,5 +3,8 @@ const colors = {
     player1: 'red',
     player2: 'blue'
 }
-
-const winningCombos = [ [], [] ]
+//what does the board look like index wise? 
+const winningCombos = [ [], [], [], [], [], [], [], [] ]
+const board =[]
+let turn;
+const endgame = ['player1 Wins', 'player2 Wins', 'Tie!']
