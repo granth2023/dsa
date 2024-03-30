39940fe169166d035e010698e5078c6ad4d76561
granth2023 <grantharris38@gmail.com>
2024-03-30 13:25:42 -0400

    squares variable added

--- src/pages/TTT/ttt.js
+++ src/pages/TTT/ttt.js
@@ -8,3 +8,5 @@ const winningCombos = [ [], [], [], [], [], [], [], [] ]
 const board =[]
 let turn;
 const endgame = ['player1 Wins', 'player2 Wins', 'Tie!']
+const squares = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
