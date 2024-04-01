f312debbaee3781c44bce53cb411e82c246ad12c
granth2023 <grantharris38@gmail.com>
2024-03-31 21:06:01 -0400

    set up ttt

--- src/pages/TTT/ttt.js
+++ src/pages/TTT/ttt.js
@@ -19,3 +19,27 @@ const squares = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 //     4.1.1) Initialize the board array to 9 nulls to represent empty squares. The 9 elements will "map" to each square, where index 0 maps to the top-left square and index 8 maps to the bottom-right square.
 //     4.1.2) Initialize whose turn it is to 1 (player 'X'). Player 'O' will be represented by -1.
 //     4.1.3) Initialize winner to null to represent that there is no winner or tie yet. Winner will hold the player value (1 or -1) if there's a winner. Winner will hold a 'T' if there's a tie. 
+
+
+
+// save x and save circle 
+// save all cells from html in an array using the dom 
+// declare a turn keeper 
+
+//on each of the cells find which cell needs to handle a click and listen for the click -- once its clicked we want to declare it on or true 
+
+//what happens when we click? what do we pass? is this an event? 
+//how do we save the cell we are targeting in this event? 
+//how do we save which x or 0 we are using based on whose turn it is? 
+//how do we take the current turn we are in and their marker and the cell they chose and place it on the mark? 
+
+// what else do we want to happen in thed handle click? 
+//how do we swap turns? 
+
+//when we pass in the place it on the m ark what are we doing? how do we modify the css to add a current class from the class list to the cell? 
+
+//how do we swap turns? how do we save circles turn? 
+//do we want to set the board hover classes on the click? what does that do? do we need to declare we are saving in board whatever element we want to get by id? 
+
+
+
