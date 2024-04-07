340d2289d3669bb9fc1083e3ab502849e779e66c
granth2023 <grantharris38@gmail.com>
2024-04-07 17:17:30 -0400

    memorize

--- src/pages/linkedlists.js
+++ src/pages/linkedlists.js
@@ -1,3 +1,18 @@
+//create n1 as an object with a key value pair of data  100 
+//repeat this process 
+//addd in a next pair on n1 and store n2 in there 
+
+//console log n1 
+//what did we just do? 
+//try something a little new 
+//meet with emre about the end of the week and be like im through on wednesday i did this whole thing for LL and then be like this is what im prepping for the next one and I'm getting code a lot more-- I think a theme for me in my adolensce through now has been over thinking coupled with an excellent memory not photographic bu tyeah. 
+
+
+//prehomewoork, review classes lab from unit 1 
+//terminiology// review that first lets review classes 
+
+
+
 // const n1 = {
 //     data: 100
 // }
@@ -10,18 +25,25 @@
 
 
 // console.log(n1);
-
+//we are making nodes as classes 
 class Node{
+    //TF we need constructor with data and next stored as null
     constructor(data, next = null){
+        //were calling this dot data whatever data is
+
         this.data = data;
+        //we're callig this dot next whatever next is //whenever we're pointing to next in the instance of this we're saying next is going to be whatever next is becuase we have a next value. sometimes its null.
         this.next = next;
+
     }
 }
 
 // const n1 = new Node(100);
 // console.log(n1)
-
+//create a class called linkedlists
 class LinkedLists {
+//can you call a constructor function that call this dot head null and this dot size zero so we have a a basically empty linked lists except its not empty it just has a size of zero and a head of null
+
     constructor() {
         this.head = null;
         this.size= 0;
@@ -31,7 +53,13 @@ class LinkedLists {
 
 
 //insert frst node
+
+//create a function called insert first and use data to store in the this.head a new node with data and a this dot head 
+//remember node is is a class with a constructor passing data and next being null with data being the the current data at this and same for next 
+//so when we run this insert first we will pass a number lets say and it will become the new node with 100 where teh data is now 100 and so this dot data is this dot data  and now were sayiang this new node is now going to be the head of? our linked list. 
     insertFirst(data){
+
+
         this.head = new Node(data, this.head);
         //add size
         this.size++;
