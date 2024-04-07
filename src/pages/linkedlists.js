9a7ea415e5affaec7e8625c7554b95e1a05df614
granth2023 <grantharris38@gmail.com>
2024-04-07 16:58:03 -0400

    insert last

--- src/pages/linkedlists.js
+++ src/pages/linkedlists.js
@@ -39,6 +39,25 @@ class LinkedLists {
 
 //insert last node
 
+    insertLast(data){
+        // this.head - new Node(data, this.head);
+        let node = new Node(data);
+        let current;
+
+        //if empty, make head 
+        if(!this.head){
+            this.head = node;
+        }else {
+            current = this.head;
+
+            while(current.next){
+                current = current.next;
+            }
+
+            current.next =node;
+        }
+    }
+
 //insert at index 
 
 // get at index
@@ -52,8 +71,11 @@ class LinkedLists {
 printListData(){
     let current = this.head; 
 
+    //while you have something in current which you derive from the head therefore you 
     while(current){
+        //make sure to see if the data can get into this while loop 
         console.log(current.data);
+        //rename what current is by saying what the current next part of the node is and send that through 
         current = current.next;
     }
 }
