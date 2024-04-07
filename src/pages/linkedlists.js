35556c9f8baca77f1018428b3f2d9e9d37d3938d
granth2023 <grantharris38@gmail.com>
2024-04-07 16:52:20 -0400

    print list data

--- src/pages/linkedlists.js
+++ src/pages/linkedlists.js
@@ -24,6 +24,45 @@ class Node{
 class LinkedLists {
     constructor() {
         this.head = null;
-        
+        this.size= 0;
+
+
+    }
+
+
+//insert frst node
+    insertFirst(data){
+        this.head = new Node(data, this.head);
+    }
+
+
+
+//insert last node
+
+//insert at index 
+
+// get at index
+
+// remove at index
+
+//clear list
+
+//print list data
+
+printListData(){
+    let current = this.head; 
+
+    while(current){
+        console.log(current.data);
+        current = current.next;
     }
 }
+}
+
+const ll = new LinkedLists();
+
+ll.insertFirst(100)
+ll.insertFirst(200)
+ll.insertFirst(300)
+
+ll.printListData();
