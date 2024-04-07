c6a5cc16bdf5e49a0d467c75c0ca5da0728372fb
granth2023 <grantharris38@gmail.com>
2024-04-07 16:46:41 -0400

    code ll

--- src/pages/linkedlists.js
+++ src/pages/linkedlists.js
@@ -1,13 +1,29 @@
-const n1 = {
-    data: 100
+// const n1 = {
+//     data: 100
+// }
+
+// const n2 ={
+//     data: 200
+// }
+
+// n1.next = n2; 
+
+
+// console.log(n1);
+
+class Node{
+    constructor(data, next = null){
+        this.data = data;
+        this.next = next;
+    }
 }
 
-const n2 ={
-    data: 200
+// const n1 = new Node(100);
+// console.log(n1)
+
+class LinkedLists {
+    constructor() {
+        this.head = null;
+        
+    }
 }
-
-n1.next = n2; 
-
-
-console.log(n1);
-
