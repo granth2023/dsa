01d9dd70d6e51dc56ba26c92c1495e72a0bf151e
granth2023 <grantharris38@gmail.com>
2024-04-25 11:18:16 -0400

    depth frist

--- src/dataStructures/trees/searchTrees.js
+++ src/dataStructures/trees/searchTrees.js
@@ -1,1 +1,5 @@
 //Breadth First is by row searching, left to right and then dipping down into anothe depth 
+
+//Depth-First Search goes like a column -- down all the way to the left and then scans to the right and snakes back up, and then across until it goe down left first 
+
+//
