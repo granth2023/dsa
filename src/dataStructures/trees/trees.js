97993b653b0fb0d95c94a8c617de8904cab3ef64
granth2023 <grantharris38@gmail.com>
2024-04-25 12:01:19 -0400

    invert tree

--- src/dataStructures/trees/trees.js
+++ src/dataStructures/trees/trees.js
@@ -65,20 +65,72 @@
 // }
 
 
-class Tree{
-    constructor(val = 0, left = null, right = null){
-        this.val = val
-        this.left=left
-        this.right = right
-    }
-}
-const maxDepth = (root) =>{
-    if(!root){
-        return 0
-    }
-    let leftDepth = maxDepth(root.left)
-    let rightDepth= maxDepth(root.right)
-    Math.max(leftDepth, rightDepth)+ 1
-}
+// class Tree{
+//     constructor(val = 0, left = null, right = null){
+//         this.val = val
+//         this.left=left
+//         this.right = right
+//     }
+// }
+// const maxDepth = (root) =>{
+//     if(!root){
+//         return 0
+//     }
+//     let leftDepth = maxDepth(root.left)
+//     let rightDepth= maxDepth(root.right)
+//     Math.max(leftDepth, rightDepth)+ 1
+// }
 
 //treemax, merge and the left right merge index and splice, binary start and end, hashmap, has the empty object and the empty array, 
+
+
+class Tree {
+    //start the value at zero and the left null and right null and declare this? what is this? 
+    constructor( val = 0, left = null, right = null){
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+//create a max depth funciton
+
+    const maxDepth = (root) =>{
+        if(!root){
+            return 0
+        }
+//
+        let leftDepth= maxDepth(root.left)
+        let rightDepth = maxDepth(root.right)
+
+       return Math.max (leftDepth, rightDepth) + 1 
+
+
+    }
+
+
+    //invert a tree 
+
+    //tree class, root === null, function that is called twice within itself swapping children, 
+
+
+    class treeNode {
+        constructor(val = 0, left = null, right = null){
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    const invertTree = (root) => {
+        if(root === null){
+            return null;
+        }
+
+        [root.left, root.right] = [ root.right, root.left]
+//what makes it traverse the tree? and go to the left os it checks to see if the thing it went to,  then you swap them with each other and keep calling on it to see if the root is not null so once root is null than we know there are no more chidlren off that node
+        invertTree(root.left)
+        invertTree(root.right)
+
+        return root  
+
+    }
